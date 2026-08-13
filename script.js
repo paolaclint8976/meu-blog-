2c78576c389b15eeb211389f51006fd662d3c48a
paolaclint8976 <paola.kovalek@escola.pr.gov.br>
2026-08-13 09:52:24 -0300

    eeee

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const botoes = document.querySelectorAll("button");
 
 botoes.forEach(botao => {
+    let curtiu = false
     botao.addEventListener("click", curtir)
 function curtir() {
     let curtidas = document.querySelector("span")
-    curtidas.textContent++
+     
+    if(se curtiu for == false){
+     curtiu = true
+     curtidas.textContent++
+     botao.style.background-color: "pink"
+    })
+
 }
 
 });
